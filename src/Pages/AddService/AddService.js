a3f8adf9e32cc4b0a94a29b5eb72bb8a9b267d73
sm-jahangir <samraatjahangir@gmail.com>
2022-05-12 01:01:39 +0600

    add services completed

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -3,23 +3,56 @@ import { useForm } from "react-hook-form";
 
 function AddService() {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    const url = "http://localhost:4000/service";
+
+    fetch(url, {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+
+    console.log(data);
+  };
   return (
     <div className="container">
       <h1>AddService</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        First Name:{" "}
-        <input {...register("firstName", { required: true, maxLength: 20 })} />{" "}
-        <br />
-        <br />
-        Last Name:{" "}
-        <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-        <br />
-        <br />
-        Age: <input type="number" {...register("age", { min: 18, max: 99 })} />
-        <br />
-        <br />
-        <input type="submit" />
+      <form className="d-flex flex-column" onSubmit={handleSubmit(onSubmit)}>
+        Name:{" "}
+        <input
+          className="mb-2 form-control"
+          placeholder="Enter Input"
+          {...register("name", { required: true, maxLength: 20 })}
+        />{" "}
+        Description:{" "}
+        <textarea
+          className="mb-2 form-control"
+          placeholder="Enter Input"
+          {...register("description")}
+        />
+        Price:{" "}
+        <input
+          className="mb-2 form-control"
+          placeholder="Enter Input"
+          type="number"
+          {...register("price")}
+        />
+        Photo URL:{" "}
+        <input
+          className="mb-2 form-control"
+          placeholder="Enter Input"
+          {...register("img")}
+        />
+        <input className="btn btn-primary mb-2" type="submit" />
       </form>
     </div>
   );
