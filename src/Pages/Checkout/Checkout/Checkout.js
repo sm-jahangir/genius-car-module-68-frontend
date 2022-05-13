25a8c814fb2e57e0ec85ad3ae81f1502973bdb84
sm-jahangir <samraatjahangir@gmail.com>
2022-05-13 09:47:55 +0600

    form ready

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,32 +1,46 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import useServiceDetail from "../../../hooks/useServiceDetail";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 
 const Checkout = () => {
   const { serviceId } = useParams();
   const [service] = useServiceDetail(serviceId);
-  const [user, setUser] = useState({
-    name: "Jahangir Alam",
-    email: "admin@mail.com",
-    service: "Ecommerce",
-    address: "Rabeya road gazipur Dhaka",
-    phone: "01767275819",
-  });
-  const handleAddressChange = (event) => {
-    console.log(event.target.value);
-    const { address, ...rest } = user;
-    const newAddress = event.target.value;
-    const newUser = { address: newAddress, ...rest };
-    setUser(newUser);
-    console.log(newUser);
+  const [user] = useAuthState(auth);
+  // const [user, setUser] = useState({
+  //   name: "Jahangir Alam",
+  //   email: "admin@mail.com",
+  //   service: "Ecommerce",
+  //   address: "Rabeya road gazipur Dhaka",
+  //   phone: "01767275819",
+  // });
+  // const handleAddressChange = (event) => {
+  //   console.log(event.target.value);
+  //   const { address, ...rest } = user;
+  //   const newAddress = event.target.value;
+  //   const newUser = { address: newAddress, ...rest };
+  //   setUser(newUser);
+  //   console.log(newUser);
+  // };
+  const handlePlaceOrder = (event) => {
+    event.preventDefault();
+    const order = {
+      service: service.name,
+      serviceId: serviceId,
+      address: event.target.address.value,
+      phone: event.target.phone.value,
+    };
+    console.log(order);
   };
   return (
     <div className="w-50 mx-auto">
       <h2>Please Checkout your booking : {service.name}</h2>
-      <form>
+      <form onSubmit={handlePlaceOrder}>
         <input
           className="form-control my-2"
-          value={user.name}
+          value={user.displayName}
+          readOnly
           type="text"
           name="name"
           placeholder="Name"
@@ -35,6 +49,8 @@ const Checkout = () => {
         <input
           className="form-control my-2"
           value={user.email}
+          disabled
+          readOnly
           type="email"
           name="email"
           placeholder="email"
@@ -42,7 +58,6 @@ const Checkout = () => {
         />
         <input
           className="form-control my-2"
-          value={user.service}
           type="text"
           name="service"
           placeholder="service"
@@ -50,16 +65,14 @@ const Checkout = () => {
         />
         <input
           className="form-control my-2"
-          value={user.address}
           type="text"
-          onChange={handleAddressChange}
           name="address"
           placeholder="address"
+          autoComplete="off"
           required
         />
         <input
           className="form-control my-2"
-          value={user.phone}
           type="text"
           name="phone"
           placeholder="phone"
